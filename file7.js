a969254eb4c2bfec080d3c7a001dda2eacfaa055
MohammadJameer2006 <mohdjam2006@gmail.com>
2026-02-16 16:40:39 +0530

    Your commit message

---
+++ file7.js
@@ -0,0 +1,13 @@
+const sample =async () =>{
+    console.log("🚀 ~ sample ~ Started:");
+
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+
+    console.log("🚀 ~ sample ~ Ended:");
+
+};
+console.log("🚀 ~ sample ~ Started---------1:");
+sample();
+console.log("🚀 ~ sample ~ Started---------2:");
